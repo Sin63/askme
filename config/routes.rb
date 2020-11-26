a1366c8109623fd4f04ce2a5adbf196092dcf90b
Sin63 <sincov63@gmail.com>
2020-11-26 21:05:18 +0300

    add sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+ 
   root 'users#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:destroy]
   resources :questions
+
+  get 'sign_up' => 'user#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 
   # get 'show' => 'users#show'
 end
