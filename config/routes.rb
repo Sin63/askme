cecebf4be9357fc377153565c9fd7c50602cb539
Sin63 <sincov63@gmail.com>
2020-12-04 15:02:56 +0300

    Added delete questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
- 
   root 'users#index'
 
-  resources :sessions, only: [:new, :create, :destroy]
+  # Ресурс пользователей (экшен destroy не поддерживается)
   resources :users, except: [:destroy]
-  resources :questions
 
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
+
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
+  #
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
   get 'log_in' => 'sessions#new'
 
-  # get 'show' => 'users#show'
+  # match "*path", :to => "errors#not_found"
+
 end
